15ce56b5cc239c1798004f87f60d6007b23dd69f
Ricardo Guzman <ricardogzm.dev@gmail.com>
2022-05-01 16:51:58 -0500

    Mejoras generales
    - Formateo de código usando black
    - Volumen disminuido
    - Textos a español
    - Arreglo de typos
    - Requirements.txt agregado

--- main.py
+++ main.py
@@ -10,27 +10,27 @@ pygame.init()
 screen = pygame.display.set_mode((800, 1000))
 
 # Background
-background = pygame.image.load('images/bg.jpg')
+background = pygame.image.load("images/bg.jpg")
 
 # Sound
 mixer.music.load("sounds/sw.wav")
-mixer.music.set_volume(.1)
+mixer.music.set_volume(0.08)
 mixer.music.play(-1)
 
 # Title and icon
 pygame.display.set_caption("Space invaders")
-icon = pygame.image.load('images/space_ship.png')
+icon = pygame.image.load("images/space_ship.png")
 pygame.display.set_icon(icon)
 
 # Score
 score_value = 0
-font = pygame.font.Font('fonts/freesansbold.ttf', 32)
+font = pygame.font.Font("fonts/freesansbold.ttf", 32)
 text_x = 10
 text_y = 50
 
 # Math operation
-font_math = pygame.font.Font('fonts/freesansbold.ttf', 20)
-font_math_operation = pygame.font.Font('fonts/freesansbold.ttf', 60)
+font_math = pygame.font.Font("fonts/freesansbold.ttf", 20)
+font_math_operation = pygame.font.Font("fonts/freesansbold.ttf", 60)
 math_x = 620
 math_y = 10
 operator_1 = random.randint(1, 10)
@@ -39,40 +39,50 @@ correct_answer_monster_pos = 0
 next_math = True
 
 # Game over text
-game_over_font = pygame.font.Font('fonts/freesansbold.ttf', 64)
+game_over_font = pygame.font.Font("fonts/freesansbold.ttf", 64)
 
 # Pause text
-pause_font = pygame.font.Font('fonts/freesansbold.ttf', 64)
+pause_font = pygame.font.Font("fonts/freesansbold.ttf", 64)
 
 # Game status
 pause_status = False
 you_loose = False
 
-def show_math(x,y):
-    operation = font_math_operation.render(str(operator_1)+ " x " + str(operator_2), True, (255, 255, 255)) 
+
+def show_math(x, y):
+    operation = font_math_operation.render(
+        str(operator_1) + " x " + str(operator_2), True, (255, 255, 255)
+    )
     screen.blit(operation, (x, y))
 
+
 def show_score(x, y):
-    score = font.render("Score: " + str(score_value), True, (255, 255, 255))
+    score = font.render("Puntaje: " + str(score_value), True, (255, 255, 255))
     screen.blit(score, (x, y))
 
 
 def game_over_text():
-    over_text = game_over_font.render("GAME OVER: " + str(score_value), True, (255, 255, 255))
-    restart_text = game_over_font.render("Press R to restart", True, (255, 255, 255))
+    over_text = game_over_font.render(
+        "GAME OVER: " + str(score_value), True, (255, 255, 255)
+    )
+    restart_text = game_over_font.render(
+        "Presiona R para reiniciar", True, (255, 255, 255)
+    )
     screen.blit(over_text, (150, 240))
-    screen.blit(restart_text, (150, 310))
+    screen.blit(restart_text, (0, 310))
 
 
 def pause_text():
-    pause_text_message = pause_font.render("P A U S E ", True, (255, 255, 255))
+    pause_text_message = pause_font.render("P A U S A", True, (255, 255, 255))
     screen.blit(pause_text_message, (230, 250))
 
+
 # Lives
-heart_img = pygame.image.load('images/heart.png')
+heart_img = pygame.image.load("images/heart.png")
 lives_x = 10
 lives_y = 10
 number_lives = 5
+
 
 def lives(x, y):
     for i in range(number_lives):
@@ -80,9 +90,8 @@ def lives(x, y):
         x += 35
 
 
-
 # Player
-player_img = pygame.image.load('images/player_icon.png')
+player_img = pygame.image.load("images/player_icon.png")
 player_x = 370
 player_y = 880
 player_x_change = 0
@@ -98,14 +107,14 @@ monster_x = []
 monster_y = []
 monster_x_change = []
 monster_y_change = []
-monster_resoult = []
+monster_result = []
 monster_values = []
 number_enemies = 5
 monster_value = 0
 dis = 50
 
 for i in range(number_enemies):
-    monster_img.append(pygame.image.load('images/monster.png'))
+    monster_img.append(pygame.image.load("images/monster.png"))
     if dis < 750:
         dis += 100
         monster_x.append(dis)
@@ -117,16 +126,16 @@ for i in range(number_enemies):
     monster_y_change.append(0.05)
     monster_value = random.randint(0, 100)
     monster_values.append(monster_value)
-    monster_resoult.append(font_math.render(str(monster_value), True, (153, 0, 0)))
+    monster_result.append(font_math.render(str(monster_value), True, (153, 0, 0)))
 
 
 def monster(x, y, i):
     screen.blit(monster_img[i], (x, y))
-    screen.blit(monster_resoult[i], (x+20, y+40))
+    screen.blit(monster_result[i], (x + 20, y + 40))
 
 
 # Bullet
-bullet_img = pygame.image.load('images/bullet.png')
+bullet_img = pygame.image.load("images/bullet.png")
 bullet_x = 0
 bullet_y = 880
 bullet_x_change = 0
@@ -169,6 +178,7 @@ while running_game:
                 if event.key == pygame.K_SPACE:
                     if not bullet_shoot:
                         bulletSound = mixer.Sound("sounds/laser.wav")
+                        bulletSound.set_volume(0.2)
                         bulletSound.play()
                         bullet_x = player_x
                         fire_bullet(bullet_x, bullet_y)
@@ -213,14 +223,14 @@ while running_game:
 
         for i in range(number_enemies):
             monster_y[i] += monster_y_change[i]
-            
+
             # Collision
             collision = is_Collision(monster_x[i], monster_y[i], bullet_x, bullet_y)
             if collision:
                 bullet_y = 880
                 bullet_shoot = False
                 if score_value == 30:
-                    player_img = pygame.image.load('images/player2_icon.png')
+                    player_img = pygame.image.load("images/player2_icon.png")
 
                 monster_x[i] = random.randint(0, 736)
                 monster_y[i] = random.randint(50, 150)
@@ -228,7 +238,7 @@ while running_game:
                 # Check if is the correct answer when collision
                 if monster_values[i] == operator_1 * operator_2:
                     explosionSound = mixer.Sound("sounds/explosion.wav")
-                    explosionSound.set_volume(5)
+                    explosionSound.set_volume(0.2)
                     explosionSound.play()
                     next_math = True
                     # update the screen
@@ -238,7 +248,7 @@ while running_game:
                     show_math(math_x, math_y)
                     monster_x = []
                     monster_y = []
-                    for i in range (number_enemies):
+                    for i in range(number_enemies):
                         if dis < 750:
                             dis += 100
                             monster_x.append(dis)
@@ -246,25 +256,37 @@ while running_game:
                             dis = 50
                             monster_x.append(dis)
                         monster_y.append(random.randint(50, 150))
-                        monster_y_change[i] += .01
+                        monster_y_change[i] += 0.01
                 else:
                     number_lives -= 1
                     explosionSound = mixer.Sound("sounds/incorrect.wav")
-                    explosionSound.set_volume(5)
+                    explosionSound.set_volume(0.2)
                     explosionSound.play()
 
-            # Define if the resoult is correct
+            # Define if the result is correct
             if next_math:
-                monster_resoult.insert(correct_answer_monster_pos, font_math.render(str(operator_1 * operator_2), True, (153, 0, 0)))
-                monster_values.insert(correct_answer_monster_pos, operator_1*operator_2)
+                monster_result.insert(
+                    correct_answer_monster_pos,
+                    font_math.render(str(operator_1 * operator_2), True, (153, 0, 0)),
+                )
+                monster_values.insert(
+                    correct_answer_monster_pos, operator_1 * operator_2
+                )
 
                 # Increase the correct answer counter
                 if correct_answer_monster_pos < number_enemies:
                     correct_answer_monster_pos += 1
                 else:
                     correct_answer_monster_pos = 0
-                    monster_resoult.insert(correct_answer_monster_pos, font_math.render(str(operator_1 * operator_2), True, (153, 0, 0)))
-                    monster_values.insert(correct_answer_monster_pos, operator_1*operator_2)
+                    monster_result.insert(
+                        correct_answer_monster_pos,
+                        font_math.render(
+                            str(operator_1 * operator_2), True, (153, 0, 0)
+                        ),
+                    )
+                    monster_values.insert(
+                        correct_answer_monster_pos, operator_1 * operator_2
+                    )
 
                 next_math = False
             monster(monster_x[i], monster_y[i], i)
